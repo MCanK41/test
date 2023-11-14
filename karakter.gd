7a04a18ec04ddf0d292a445e871fd2dad8233c19
MCanK <100578225+MCanK41@users.noreply.github.com>
2023-11-14 23:22:26 +0300

    VGHHM

--- karakter.gd
+++ karakter.gd
@@ -1,28 +1,25 @@
 extends CharacterBody2D
 
+@export var speed = 200
+@export var friction = 0.1
+@export var acceleration = 0.7
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 500
-
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('right'):
+		input.x += 10
+	if Input.is_action_pressed('left'):
+		input.x -= 10
+	if Input.is_action_pressed('down'):
+		input.y += 10
+	if Input.is_action_pressed('up'):
+		input.y -= 10
+	return input
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = velocity.lerp(Vector2.ZERO, friction)
 	move_and_slide()
